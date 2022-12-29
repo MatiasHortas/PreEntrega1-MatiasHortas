a9f489a3124eeac6ea44a9895ad4725a1287b880
Matias Hortas <matiasagustinhortas@gmail.com>
2022-12-29 20:57:09 -0300

    mi primer commit

---
+++ scrips/primerentrega.js
@@ -0,0 +1,102 @@
+// variables
+
+const usuario = "matih";
+const contrasena = "123m";
+const pressbanca = 30;
+const flexiones = 15;
+const apertumancu = 20;
+const curlbiceps = 15;
+var oncod = true;
+//presentacion
+alert("Bienvenido a FITPLAN. \n -Aqui podras hacer tu seguimiento de gimnasio y ver tus mejoras.")
+
+//login de cuenta con 3 reintentos
+for (i = 3; i >= 0; i--) {
+    let u = prompt("Ingresa tu usuario");
+    let c = prompt("Ingresa tu contraseña");
+    if ((u == usuario) && (c == contrasena)) {
+        alert("Bienvenido Admin")
+        break;
+    }
+    if (u == "" && c == "") {
+        alert("No ingresaste nada, porfavor ingresa tu usuario y contraseña. Te quedan " + i + " intentos")
+    } else {
+        alert("Usuario y contraseña incorrecta, vuelve a intentar. Te quedan " + i + " Intentos")
+    }
+    if (i == 0) {
+        alert("Tu cuenta fue bloqueada, comunicate con soporte.")
+
+
+        break;
+        // si se llega a los limites de reintentos se bloquea la cuenta y se corta el siguiente codigo cambia la var oncod a false.
+    }
+    var oncod = false;
+
+}
+
+
+//
+while (oncod == true) {
+    var menua = prompt("Bienvenido " + usuario + "  \n\ 1 Seguimiento de ejercicios \n\  2 salir.")
+
+
+    switch (menua) {
+        case "1":
+            alert(usuario + " espero que hayas tenido una mejora optima")
+            break;
+        case "2":
+            alert("Muchos exitos en tu entrenamiento " + usuario)
+            block;
+        default:
+            alert("Numero ingresado incorrecto, porfavor ingresar \n\ 1 Seguimiento de ejercicios  \n\ 2 salir. ")
+            continue;
+    }
+
+
+
+
+    let pressb = Number(prompt("Cuantos kilos levantaste en pressbanca"))
+    let flex = Number(prompt("Cuantos kilos levantaste en flexiones"))
+    let apertm = Number(prompt("Cuantos kilos levantaste en apertura con mancuerna"))
+    let curlb = Number(prompt("Cuantos kilos levantaste en curlbiceps"))
+
+    if (isNaN(pressb) || isNaN(flex) || isNaN(apertm) || isNaN(curlb) || pressb <= 0 || flex <= 0 || apertm <= 0 || curlb <= 0) {
+        alert("Porfavor ingresar numeros validos, muchas gracias.");
+        continue;
+
+    }
+
+    if (pressb > pressbanca) {
+        alert("Mejoraste tu peso de pressbanca, estabas levantando " + pressbanca + " kilos y ahora levantaste " + pressb)
+    } else {
+        alert("No mejoraste tu anterior peso de pressbanca, estabas levantando " + pressbanca + " kilos y ahora levantaste " + pressb + " sigue esforzandote")
+    }
+
+    if (flex > flexiones) {
+        alert("Mejoraste tu peso de flexiones, estabas levantando " + flexiones + " kilos y ahora levantaste " + flex)
+    } else {
+        alert("No mejoraste tu anterior peso de flexiones, estabas levantando " + flexiones + " kilos y ahora levantaste " + flex + " sigue esforzandote")
+    }
+    if (apertm > apertumancu) {
+        alert("Mejoraste tu peso de apertura con mancuernas, estabas levantando " + apertumancu + " kilos y ahora levantaste " + apertm)
+    } else {
+        alert("No mejoraste tu anterior peso de apertura con mancuernas, estabas levantando " + apertumancu + " kilos y ahora levantaste " + apertm + " sigue esforzandote")
+    }
+    if (curlb > curlbiceps) {
+        alert("Mejoraste tu peso de curlbiceps, estabas levantando " + curlbiceps + " kilos y ahora levantaste " + curlb)
+    } else {
+        alert("No mejoraste tu anterior peso de curlbiceps, estabas levantando " + curlbiceps + " kilos y ahora levantaste " + curlb + " sigue esforzandote")
+    }
+
+    // Function en flecha
+    const kilosAumentado = (pressb, pressbanca) => pressb - pressbanca;
+    const kilosAumentado1 = (apertm, apertumancu) => apertm - apertumancu;
+    const kilosAumentado2 = (flex, flexiones) => flex - flexiones;
+    const kilosAumentado3 = (curlb, curlbiceps) => curlb - curlbiceps;
+
+
+    // alerta de cuanto kilos mejoraste
+    alert("Aumentaste " + kilosAumentado(pressb,pressbanca) + " Kilos en pressbanca \n" + "Aumentaste " + kilosAumentado1(apertm, apertumancu) + " Kilos en apertura con mancuernas \n" + "Aumentaste " + kilosAumentado2(flex, flexiones) + " Kilos en flexiones \n" + "Aumentaste " + kilosAumentado3(curlb, curlbiceps) + " Kilos en curlb de biceps \n")
+
+}
+
