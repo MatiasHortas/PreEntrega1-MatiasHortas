03b31f2311274bb6ef766e17221bbfbe227fb608
Matias Hortas <matiasagustinhortas@gmail.com>
2023-01-19 22:53:38 -0300

    segunda entrega

--- scrips/primerentrega.js
+++ scrips/primerentrega.js
@@ -1,3 +1,12 @@
+//  Constructor
+class Gym {
+    constructor(ejercicio, repeticiones, kilos) {
+        this.ejercicio = ejercicio;
+        this.repeticiones = repeticiones;
+        this.kilos = Number(kilos);
+    }
+}
+
 // variables
 
 const usuario = "matih";
@@ -7,6 +16,10 @@ const flexiones = 15;
 const apertumancu = 20;
 const curlbiceps = 15;
 var oncod = true;
+let next;
+const nuevoRegistro = [];
+
+
 //presentacion
 alert("Bienvenido a FITPLAN. \n -Aqui podras hacer tu seguimiento de gimnasio y ver tus mejoras.")
 
@@ -37,66 +50,82 @@ for (i = 3; i >= 0; i--) {
 
 //
 while (oncod == true) {
-    var menua = prompt("Bienvenido " + usuario + "  \n\ 1 Seguimiento de ejercicios \n\  2 salir.")
+    var menua = prompt("Bienvenido " + usuario + "  \n\ 1 Seguimiento de ejercicios \n\  2 Ejercicios anotados. \n\ 3 salir ")
 
 
     switch (menua) {
         case "1":
-            alert(usuario + " espero que hayas tenido una mejora optima")
+            let registroEjercicio = prompt("Que ejercicio realizaste?")
+            let registrorepeticiones = Number(prompt("Cuantas series hiciste"))
+            let registroKilos = Number(prompt("Cuantos kilos levantaste"))
+            let listaregistronuevo = new Gym(registroEjercicio, registrorepeticiones, registroKilos)
+            nuevoRegistro.push(listaregistronuevo)
+            next = confirm("Quieres ingresar otro registro de ejercicios?");
             break;
+
+
         case "2":
+            const filtradokilos = nuevoRegistro.map((Element) => Element.kilos)
+            const filtradoEjercicio = nuevoRegistro.map((Element) => Element.ejercicio)
+            const filtradoRepeticiones = nuevoRegistro.map((Element) => Element.repeticiones)
+            alert("En el ejercicio " + filtradoEjercicio + " realizaste " + filtradoRepeticiones + " Repeticiones con " + filtradokilos + " Kilos")
+            break;
+
+
+        case "3":
             alert("Muchos exitos en tu entrenamiento " + usuario)
+
             block;
         default:
-            alert("Numero ingresado incorrecto, porfavor ingresar \n\ 1 Seguimiento de ejercicios  \n\ 2 salir. ")
+            alert("Numero ingresado incorrecto, porfavor ingresar \n\ 1 Seguimiento de ejercicios \n\  2 Ejercicios anotados. \n\ 3 salir ")
             continue;
     }
 
-
-
-
-    let pressb = Number(prompt("Cuantos kilos levantaste en pressbanca"))
-    let flex = Number(prompt("Cuantos kilos levantaste en flexiones"))
-    let apertm = Number(prompt("Cuantos kilos levantaste en apertura con mancuerna"))
-    let curlb = Number(prompt("Cuantos kilos levantaste en curlbiceps"))
-
-    if (isNaN(pressb) || isNaN(flex) || isNaN(apertm) || isNaN(curlb) || pressb <= 0 || flex <= 0 || apertm <= 0 || curlb <= 0) {
-        alert("Porfavor ingresar numeros validos, muchas gracias.");
-        continue;
-
-    }
-
-    if (pressb > pressbanca) {
-        alert("Mejoraste tu peso de pressbanca, estabas levantando " + pressbanca + " kilos y ahora levantaste " + pressb)
-    } else {
-        alert("No mejoraste tu anterior peso de pressbanca, estabas levantando " + pressbanca + " kilos y ahora levantaste " + pressb + " sigue esforzandote")
-    }
-
-    if (flex > flexiones) {
-        alert("Mejoraste tu peso de flexiones, estabas levantando " + flexiones + " kilos y ahora levantaste " + flex)
-    } else {
-        alert("No mejoraste tu anterior peso de flexiones, estabas levantando " + flexiones + " kilos y ahora levantaste " + flex + " sigue esforzandote")
-    }
-    if (apertm > apertumancu) {
-        alert("Mejoraste tu peso de apertura con mancuernas, estabas levantando " + apertumancu + " kilos y ahora levantaste " + apertm)
-    } else {
-        alert("No mejoraste tu anterior peso de apertura con mancuernas, estabas levantando " + apertumancu + " kilos y ahora levantaste " + apertm + " sigue esforzandote")
-    }
-    if (curlb > curlbiceps) {
-        alert("Mejoraste tu peso de curlbiceps, estabas levantando " + curlbiceps + " kilos y ahora levantaste " + curlb)
-    } else {
-        alert("No mejoraste tu anterior peso de curlbiceps, estabas levantando " + curlbiceps + " kilos y ahora levantaste " + curlb + " sigue esforzandote")
-    }
-
-    // Function en flecha
-    const kilosAumentado = (pressb, pressbanca) => pressb - pressbanca;
-    const kilosAumentado1 = (apertm, apertumancu) => apertm - apertumancu;
-    const kilosAumentado2 = (flex, flexiones) => flex - flexiones;
-    const kilosAumentado3 = (curlb, curlbiceps) => curlb - curlbiceps;
-
-
-    // alerta de cuanto kilos mejoraste
-    alert("Aumentaste " + kilosAumentado(pressb,pressbanca) + " Kilos en pressbanca \n" + "Aumentaste " + kilosAumentado1(apertm, apertumancu) + " Kilos en apertura con mancuernas \n" + "Aumentaste " + kilosAumentado2(flex, flexiones) + " Kilos en flexiones \n" + "Aumentaste " + kilosAumentado3(curlb, curlbiceps) + " Kilos en curlb de biceps \n")
-
 }
 
+
+
+//     let pressb = Number(prompt("Cuantos kilos levantaste en pressbanca"))
+//     let flex = Number(prompt("Cuantos kilos levantaste en flexiones"))
+//     let apertm = Number(prompt("Cuantos kilos levantaste en apertura con mancuerna"))
+//     let curlb = Number(prompt("Cuantos kilos levantaste en curlbiceps"))
+
+//     if (isNaN(pressb) || isNaN(flex) || isNaN(apertm) || isNaN(curlb) || pressb <= 0 || flex <= 0 || apertm <= 0 || curlb <= 0) {
+//         alert("Porfavor ingresar numeros validos, muchas gracias.");
+//         continue;
+
+//     }
+
+//     if (pressb > pressbanca) {
+//         alert("Mejoraste tu peso de pressbanca, estabas levantando " + pressbanca + " kilos y ahora levantaste " + pressb)
+//     } else {
+//         alert("No mejoraste tu anterior peso de pressbanca, estabas levantando " + pressbanca + " kilos y ahora levantaste " + pressb + " sigue esforzandote")
+//     }
+
+//     if (flex > flexiones) {
+//         alert("Mejoraste tu peso de flexiones, estabas levantando " + flexiones + " kilos y ahora levantaste " + flex)
+//     } else {
+//         alert("No mejoraste tu anterior peso de flexiones, estabas levantando " + flexiones + " kilos y ahora levantaste " + flex + " sigue esforzandote")
+//     }
+//     if (apertm > apertumancu) {
+//         alert("Mejoraste tu peso de apertura con mancuernas, estabas levantando " + apertumancu + " kilos y ahora levantaste " + apertm)
+//     } else {
+//         alert("No mejoraste tu anterior peso de apertura con mancuernas, estabas levantando " + apertumancu + " kilos y ahora levantaste " + apertm + " sigue esforzandote")
+//     }
+//     if (curlb > curlbiceps) {
+//         alert("Mejoraste tu peso de curlbiceps, estabas levantando " + curlbiceps + " kilos y ahora levantaste " + curlb)
+//     } else {
+//         alert("No mejoraste tu anterior peso de curlbiceps, estabas levantando " + curlbiceps + " kilos y ahora levantaste " + curlb + " sigue esforzandote")
+//     }
+
+//     // Function en flecha
+//     const kilosAumentado = (pressb, pressbanca) => pressb - pressbanca;
+//     const kilosAumentado1 = (apertm, apertumancu) => apertm - apertumancu;
+//     const kilosAumentado2 = (flex, flexiones) => flex - flexiones;
+//     const kilosAumentado3 = (curlb, curlbiceps) => curlb - curlbiceps;
+
+
+//     // alerta de cuanto kilos mejoraste
+//     alert("Aumentaste " + kilosAumentado(pressb, pressbanca) + " Kilos en pressbanca \n" + "Aumentaste " + kilosAumentado1(apertm, apertumancu) + " Kilos en apertura con mancuernas \n" + "Aumentaste " + kilosAumentado2(flex, flexiones) + " Kilos en flexiones \n" + "Aumentaste " + kilosAumentado3(curlb, curlbiceps) + " Kilos en curlb de biceps \n")
+
+// }
